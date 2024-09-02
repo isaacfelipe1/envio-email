900d50df82c7af224e8b09e3e6cc89fc68621f3a
isaacfelipe1 <ifdsl.lic20@uea.edu.br>
2024-09-02 05:33:25 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Mail;
+using System.Net;
+
+public class Program
+{
+
+    static void Main(string[] args)
+    {
+        string smtpAddress = "smtp.gmail.com";
+        int portNumber = 587; 
+        bool enableSSL = true;        
+        string emailFrom = " Seu e-mail";
+        string password = " Sua senha"; 
+        string emailTo = " E-mail do destinatário";
+        string subject = "Assunto do E-mail";
+        string body = " Corpo do E-mail";
+
+        using (MailMessage mail = new MailMessage())
+        {
+            mail.From = new MailAddress(emailFrom);
+            mail.To.Add(emailTo);
+            mail.Subject = subject;
+            mail.Body = body;
+            mail.IsBodyHtml = true;
+
+            using (SmtpClient smtp = new SmtpClient(smtpAddress, portNumber))
+            {
+                smtp.Credentials = new NetworkCredential(emailFrom, password);
+                smtp.EnableSsl = enableSSL;
+                try
+                {
+                    smtp.Send(mail);
+                    Console.WriteLine("E-mail enviado com sucesso!");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Erro ao enviar e-mail: " + ex.Message);
+                }
+            }
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,6 @@
+
+# Envio de Email com C#
+
+Este é um projeto de exemplo que envia um email utilizando o framework .NET 8.0 e o provedor de email SMTP do Gmail. 
+
+
